f0adcd87337ca812827795fadb5663b9250eba77
SevaM1988 <markov.vsevolod@inbox.ru>
2018-10-29 15:41:47 +0300

    Lesson 6(#1)

--- Lesson 6/public_html/calculator.php
+++ Lesson 6/public_html/calculator.php
@@ -6,6 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <style>
+        label   {
+            margin: 15px;
+            text-align: center;
+        }
+        form    {
+            display: flex;
+            justify-content: center;
+        }
+    </style>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
@@ -56,14 +66,16 @@
     ?>
 
     <form method="post" action="calculator.php">
-        <input name="number1" type="text" class="form-control" style="width: 150px; display: inline" />
-        <select name="operation">
-            <option value="plus">Plus</option>
-            <option value="minus">Minus</option>
-            <option value="multiply">Multiply</option>
-            <option value="divided by">Divided By</option>
-        </select>
-        <input name="number2" type="text" class="form-control" style="width: 150px; display: inline" />
+        <label>Первое число<br><input name="number1" type="text" class="form-control" style="width: 150px;" /></label>
+        <label>Математические операции<br>
+            <select name="operation">
+                <option value="plus">Plus</option>
+                <option value="minus">Minus</option>
+                <option value="multiply">Multiply</option>
+                <option value="divided by">Divided By</option>
+            </select>
+        </label>
+        <label>Второе число<br><input name="number2" type="text" class="form-control" style="width: 150px;" /></label>
         <input name="submit" type="submit" value="Calculate" class="btn btn-primary" />
     </form>
 
