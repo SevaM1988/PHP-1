468548dd8fe24cf2ae72ba8f9c77947091629987
SevaM1988 <markov.vsevolod@inbox.ru>
2018-10-29 11:19:06 +0300

    Lesson 6

--- Lesson 5/Database/gallery.sql
+++ Lesson 5/Database/gallery.sql
@@ -1,6 +1,6 @@
 use Gallery;
 
-INSERT into Img (id, size, name, src) VALUE (1, '600x600', 'original1.jpg', 'Img/Big');
+INSERT into Img (size, name, src) VALUE ('600x600', 'original1.jpg', 'Img/Big');
 INSERT into Img (size, name, src) VALUE ('600x600', 'original2.jpg', 'Img/Big');
 INSERT into Img (size, name, src) VALUE ('600x600', 'original3.jpg', 'Img/Big');
 
